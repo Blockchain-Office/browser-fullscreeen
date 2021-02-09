89e0ef8248790f7fefd68066429501996a9c1ef4
Blockchain Office <71699794+Blockchain-Office@users.noreply.github.com>
2021-02-09 15:47:58 +0100

    Create screen.js

---
+++ screen.js
@@ -0,0 +1,28 @@
+function enterFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+}
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+function FULL(){
+  if((window.fullScreen) ||
+   (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
+     exitFullscreen(document.documentElement);
+   } else {
+     enterFullscreen(document.documentElement);
+   }
+}
